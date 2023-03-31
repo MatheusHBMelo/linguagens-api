a4040ef61cbfa765c4f04078455bb6ad4e303fe8
MatheusHBMelo <matheushbmelo@gmail.com>
2023-03-31 20:11:07 -0300

    [Feat] Create the method findById

--- src/main/java/br/com/theuz/linguagensapi/controller/LinguagemController.java
+++ src/main/java/br/com/theuz/linguagensapi/controller/LinguagemController.java
@@ -3,8 +3,11 @@ package br.com.theuz.linguagensapi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.com.theuz.linguagensapi.model.Linguagem;
 import br.com.theuz.linguagensapi.repository.LinguagemRepository;
@@ -19,5 +22,10 @@ public class LinguagemController {
     public List<Linguagem> findAllLanguage(){
         List<Linguagem> linguagens = repositorio.findByOrderByRanking();
         return linguagens;
+    }
+
+    @GetMapping(value="/linguagens/{id}")
+    public Linguagem findById(@PathVariable String id){
+        return repositorio.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 }
