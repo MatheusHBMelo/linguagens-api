18ae5fa540fdbb41eecfcc5b700f4509834b5a88
MatheusHBMelo <matheushbmelo@gmail.com>
2023-03-31 19:55:25 -0300

    [Feat] Create the method findByOrderByRanking

--- src/main/java/br/com/theuz/linguagensapi/repository/LinguagemRepository.java
+++ src/main/java/br/com/theuz/linguagensapi/repository/LinguagemRepository.java
@@ -1,9 +1,11 @@
 package br.com.theuz.linguagensapi.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import br.com.theuz.linguagensapi.model.Linguagem;
 
 public interface LinguagemRepository extends MongoRepository<Linguagem, String> {
-    
+    List<Linguagem> findByOrderByRanking();
 }
