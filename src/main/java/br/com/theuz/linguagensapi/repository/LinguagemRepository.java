d57dc36e003010508113a65c254fbe5538b8b3cb
MatheusHBMelo <matheushbmelo@gmail.com>
2023-03-31 20:24:45 -0300

    [Feat] Create the method existsByRanking

--- src/main/java/br/com/theuz/linguagensapi/repository/LinguagemRepository.java
+++ src/main/java/br/com/theuz/linguagensapi/repository/LinguagemRepository.java
@@ -9,4 +9,5 @@ import br.com.theuz.linguagensapi.model.Linguagem;
 public interface LinguagemRepository extends MongoRepository<Linguagem, String> {
     List<Linguagem> findByOrderByRanking();
     boolean existsByTitle(String title);
+    boolean existsByRanking(Integer ranking);
 }
